111bfcaf0fb6aa19e879c8a9067d721cd33f83d5
Murdho Savila <murdho@murdho.com>
2024-12-23 00:32:15 +0200

    day 5 part 2 rust

--- 2024_rust/src/day05.rs
+++ 2024_rust/src/day05.rs
@@ -1,48 +1,74 @@
 use std::collections::HashSet;
 
-const INPUT: &str = include_str!("../../2024_inputs/day05.txt");
-// const INPUT: &str = include_str!("../../2024_inputs/day05_example.txt");
+// const INPUT: &str = include_str!("../../2024_inputs/day05.txt");
+const INPUT: &str = include_str!("../../2024_inputs/day05_example.txt");
 
-fn are_valid_pages_for_update(rules: &HashSet<(&str, &str)>, pages: &Vec<&str>) -> bool {
-    for (i, left) in pages.iter().enumerate() {
-        for right in pages[i..].iter() {
-            if rules.contains(&(right, left)) {
-                return false;
-            }
-        }
-    }
+type Rules<'a> = HashSet<(&'a str, &'a str)>;
 
-    true
-}
+type Update<'a> = Vec<&'a str>;
+type Updates<'a> = Vec<Update<'a>>;
 
 pub fn part_one() -> i32 {
-    let input_parts: Vec<_> = INPUT.split("\n\n").collect();
+    let (rules, updates) = read_input();
 
-    let page_ordering_rules: HashSet<(&str, &str)> =
-        input_parts[0]
-            .split("\n")
-            .fold(HashSet::new(), |mut acc, rule| {
-                let pages: Vec<_> = rule.split("|").collect();
-                acc.insert((pages[0], pages[1]));
-                acc
-            });
-
-    let pages_for_updates: Vec<_> = input_parts[1]
-        .split("\n")
-        .map(|pages| pages.split(",").collect::<Vec<_>>())
-        .collect();
-
-    let valid_updates: Vec<_> = pages_for_updates
+    let valid_updates: Vec<_> = updates
         .into_iter()
-        .filter(|pages| are_valid_pages_for_update(&page_ordering_rules, pages))
+        .filter(|pages| is_valid_update(&rules, pages))
         .collect();
 
-    valid_updates
-        .iter()
-        .map(|coll| coll[coll.len() / 2].parse::<i32>().unwrap())
-        .sum()
+    sum_centers(valid_updates)
 }
 
 pub fn part_two() -> i32 {
-    -1
+    let (rules, updates) = read_input();
+
+    let invalid_updates: Updates = updates
+        .into_iter()
+        .filter(|update| !is_valid_update(&rules, update))
+        .map(|update| reorder_update(&rules, update))
+        .collect();
+
+    sum_centers(invalid_updates)
+}
+
+fn read_input<'a>() -> (Rules<'a>, Updates<'a>) {
+    let input_parts: Vec<_> = INPUT.split("\n\n").collect();
+
+    let rules: Rules = input_parts[0]
+        .split("\n")
+        .fold(HashSet::new(), |mut acc, rule| {
+            let pages: Vec<_> = rule.split("|").collect();
+            acc.insert((pages[0], pages[1]));
+            acc
+        });
+
+    let updates: Updates = input_parts[1]
+        .split("\n")
+        .map(|pages| pages.split(",").collect::<Update>())
+        .collect();
+
+    (rules, updates)
+}
+
+fn is_valid_update(rules: &Rules, update: &Update) -> bool {
+    !update.iter().enumerate().any(|(i, left)| {
+        update[i..]
+            .iter()
+            .any(|right| rules.contains(&(right, left)))
+    })
+}
+
+fn reorder_update<'a>(rules: &Rules<'a>, mut update: Update<'a>) -> Update<'a> {
+    update.sort_by(|a, b| match rules.contains(&(a, b)) {
+        true => std::cmp::Ordering::Less,
+        false => std::cmp::Ordering::Greater,
+    });
+    update
+}
+
+fn sum_centers(updates: Updates) -> i32 {
+    updates
+        .iter()
+        .map(|coll| coll[coll.len() / 2].parse::<i32>().unwrap())
+        .sum()
 }
